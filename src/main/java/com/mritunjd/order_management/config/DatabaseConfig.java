cc6c7cf3f7e6ba89ae961a958646494c0b23ea10
MD Dubey <mdinfotech007@gmail.com>
2014-12-30 09:22:17 +0530

    Written First Integration Test for Customer Repository.

--- src/main/java/com/mritunjd/order_management/config/DatabaseConfig.java
+++ src/main/java/com/mritunjd/order_management/config/DatabaseConfig.java
@@ -1,14 +1,15 @@
 package com.mritunjd.order_management.config;
 
-import com.mysql.jdbc.Driver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
 import javax.sql.DataSource;
+import java.sql.Driver;
 
 @Configuration
 public class DatabaseConfig {
@@ -17,17 +18,23 @@ public class DatabaseConfig {
     private Environment environment;
 
     @Bean
-    public DataSource dataSource() {
-        SimpleDriverDataSource basicDataSource = new SimpleDriverDataSource();
-        basicDataSource.setUrl(environment.getProperty("DATABASE_URL"));
-        basicDataSource.setUsername(environment.getProperty("DATABASE_USER"));
-        basicDataSource.setPassword(environment.getProperty("DATABASE_PASSWORD"));
-        basicDataSource.setDriverClass(Driver.class);
-        return basicDataSource;
+    public DataSource dataSource() throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+        String database_url = environment.getProperty("DATABASE_URL");
+        String database_user = environment.getProperty("DATABASE_USER");
+        String database_password = environment.getProperty("DATABASE_PASSWORD");
+        Driver driver=(Driver)Class.forName(environment.getProperty("DATABASE_DRIVER")).newInstance();
+
+        SimpleDriverDataSource dataSource=new SimpleDriverDataSource(driver,database_url,database_user,database_password);
+        return dataSource;
     }
 
     @Bean
-    public JdbcTemplate jdbcTemplate() {
+    public JdbcTemplate jdbcTemplate() throws IllegalAccessException, InstantiationException, ClassNotFoundException {
         return new JdbcTemplate(dataSource());
+    }
+
+    @Bean
+    public DataSourceTransactionManager transactionManager() throws IllegalAccessException, InstantiationException, ClassNotFoundException {
+        return new DataSourceTransactionManager(dataSource());
     }
 }
--- src/test/java/com/mritunjd/order_management/repository/CustomerRepositoryIntegrationTest.java
+++ src/test/java/com/mritunjd/order_management/repository/CustomerRepositoryIntegrationTest.java
@@ -1,19 +1,49 @@
 package com.mritunjd.order_management.repository;
 
 import com.mritunjd.order_management.config.DatabaseConfig;
+import com.mritunjd.order_management.model.Customer;
+import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @TestPropertySource("/test-om.properties")
 @ContextConfiguration(classes = {DatabaseConfig.class, TestConfig.class})
-
+@WebAppConfiguration
 public class CustomerRepositoryIntegrationTest {
-    @Test
-    public void shouldGiveACustomer() {
 
+    @Autowired
+    private CustomerRepository repository;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @After
+    public void tearDown() {
+        jdbcTemplate.update("delete from customers");
     }
+
+
+    @Test
+    public void shouldGiveACustomer() throws SQLException {
+        String setUpSql = "create table customers as SELECT * FROM CSVREAD('classpath:/csvs/customers.csv') ";
+        jdbcTemplate.execute(setUpSql);
+
+        List<Customer> customers = repository.getCustomerById("1");
+        System.out.println(customers);
+        assertEquals(1,customers.size());
+    }
+
+
 }
